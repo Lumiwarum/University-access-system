7c6afb445c9dd18b69ed062164b0788c7662fcaf
PUTIN-POMOGI <78841564+PUTIN-POMOGI@users.noreply.github.com>
2021-03-07 20:58:01 +0300

    Add files via upload

---
+++ BojeSPasi/cmake-build-debug/CMakeFiles/BojeSPasi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BojeSPasi.exe"
+  "BojeSPasi.exe.manifest"
+  "BojeSPasi.pdb"
+  "CMakeFiles/BojeSPasi.dir/main.cpp.obj"
+  "libBojeSPasi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BojeSPasi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
